9b432581a9a30e89513be7f0aa3e6e000cc718aa
andreyshilov0 <an.schilow@yandex.ru>
2023-05-12 11:05:26 +0300

    Add memo

--- app/services/tmdb_api.rb
+++ app/services/tmdb_api.rb
@@ -3,37 +3,31 @@ class TmdbApi
   INSTANCE_URL = ENV['INSTANCE_URL']
 
   def instance_faraday_url
-    Faraday.new(url: INSTANCE_URL, params: { api_key: API_KEY, language: 'en-US' }, headers: { 'Content-Type' => 'application/json' })
+    @instance_faraday_url ||= Faraday.new(url: INSTANCE_URL, params: { api_key: API_KEY, language: 'en-US' },
+                                          headers: { 'Content-Type' => 'application/json' })
   end
 
   def get_api_data(url, params = {})
     response_api_data = instance_faraday_url.get(url, params)
     data = JSON.parse(response_api_data.body || '{}')
-    @data = data
-    
   rescue JSON::ParserError, TypeError => e
     logger.info(e)
   end
 
   def genres_movie
     response_api_data = get_api_data('genre/movie/list')
-    @response_api_data = response_api_data
   end
-  
+
   def discover_movie(sort_by, page, with_genres, year, vote_average)
     response_api_data = get_api_data('discover/movie', { 'sort_by' => sort_by, 'page' => page, 'with_genres' => with_genres, 'year' => year,
-      'vote_average' => vote_average })
-    @response_api_data = response_api_data
-      
+                                                         'vote_average' => vote_average })
   end
 
   def movie_by_id(id)
     response_api_data = get_api_data("movie/#{id}")
-    @response_api_data = response_api_data
   end
 
   def total_pages
-    response_api_data = get_api_data('discover/movie')
-    @response_api_data = response_api_data['total_pages']
+    response_api_data = get_api_data('discover/movie', { 'total_pages' => 'total_pages' })
   end
 end
