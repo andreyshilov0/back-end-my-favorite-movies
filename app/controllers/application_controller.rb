ae42fada993638985b1494627ca18dcd985b5dda
andreyshilov0 <an.schilow@yandex.ru>
2023-05-19 17:40:06 +0300

    Delete comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@
 class ApplicationController < ActionController::API
   def self.current_user
     User.find_by(id: 1)
-    # user1 = User.new(id: 1, login:'Jack')
-    # genre1 = Genre.new(id: 1, title: 'Action')
-    # data = user1.genres.build
-    # binding.pry
+
   end
 end
