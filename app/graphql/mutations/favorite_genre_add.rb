368d027ed05caa2da608cc8eaa68cd139ce083d9
andreyshilov0 <an.schilow@yandex.ru>
2023-05-23 14:45:46 +0300

    Fix names

--- app/graphql/mutations/favorite_genre_add.rb
+++ app/graphql/mutations/favorite_genre_add.rb
@@ -6,10 +6,10 @@ module Mutations
     field :errors, Types::ValidationErrorsType, null: true
 
     def resolve(id:)
-      index_genres_id = id.to_i
+      index_genres = id.to_i
       current_user = context[:current_user]
-      favorite_genres = tmdb_api.genres_movie['genres']
-      genre_parameters = favorite_genres.fetch(index_genres_id)
+      all_received_genres = tmdb_api.genres_movie['genres']
+      genre_parameters = all_received_genres.fetch(index_genres)
       favorite_genre = current_user.genres.build(id: genre_parameters['id'], title: genre_parameters['name'])
 
       return if favorite_genre.save
