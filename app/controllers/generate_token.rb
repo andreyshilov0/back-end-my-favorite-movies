981d8e8f497fe3c0b57d99c24d2e8ddd20658e1b
andreyshilov0 <an.schilow@yandex.ru>
2023-06-13 16:18:56 +0300

    add env algo

--- app/controllers/generate_token.rb
+++ app/controllers/generate_token.rb
@@ -9,13 +9,13 @@ class GenerateToken < ApplicationController
     return true if user
 
     def encode_user_data(payload)
-      JWT.encode payload, Rails.application.credentials.fetch(:secret_key_base), 'HS256'
+      JWT.encode payload, Rails.application.credentials.fetch(:secret_key_base), { algorithm: ENV['HMAC_ALGORITHM'] }
     rescue StandardError => e
       logger.info e
     end
 
     def decode_user_data(token)
-      JWT.encode token, Rails.application.credentials.fetch(:secret_key_base), true, { algorithm: 'HS256' }
+      JWT.encode token, Rails.application.credentials.fetch(:secret_key_base), true, { algorithm: ENV['HMAC_ALGORITHM'] }
     rescue StandardError => e
       logger.info e
     end
